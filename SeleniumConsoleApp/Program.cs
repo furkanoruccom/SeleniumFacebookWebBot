289df895d923d60461b87911d781b0f00389362c
Furkan Oruç <20211129072@ogr.akdeniz.edu.tr>
2023-10-09 13:14:06 +0300

    update

--- SeleniumConsoleApp/Program.cs
+++ SeleniumConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
 
@@ -12,8 +13,14 @@ namespace WebBotExample
 
         static void Main(string[] args)
         {
+
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("--disable-notifications");
+            options.AddUserProfilePreference("profile.default_content_setting_values.notifications", 2);
+
             using (IWebDriver driver = new ChromeDriver())
             {
+
                 driver.Navigate().GoToUrl("https://www.facebook.com/");
 
                 WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
@@ -27,14 +34,19 @@ namespace WebBotExample
                 IWebElement formElement = WaitUntilElementVisible(driver, By.CssSelector("form._9vtf"), 20);
                 formElement.Submit();
 
+                System.Threading.Thread.Sleep(5000);
                 driver.Navigate().GoToUrl("https://www.facebook.com/thefurkanoruc/posts/1913940275667135:1913940275667135");
+
+
+
+
                 wait.Until(driver => ((IJavaScriptExecutor)driver).ExecuteScript("return document.readyState").Equals("complete"));
 
 
                 IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
-                //js.ExecuteScript("window.location= 'https://www.facebook.com/thefurkanoruc/posts/1913940275667135:1913940275667135'");
 
-                // Elementin sayfada olup olmadığını kontrol edin.
+
+                // Elementin sayfada olup olmadığı kontrol
                 wait.Until(ExpectedConditions.ElementExists(By.CssSelector("form.x1ed109x div.xzsf02u")));
                 wait.Until(ExpectedConditions.ElementExists(By.CssSelector("form.x1ed109x")));
                 wait.Until(ExpectedConditions.ElementExists(By.CssSelector("form.x1ed109x p.xdj266r")));
@@ -43,7 +55,10 @@ namespace WebBotExample
                 js.ExecuteScript("document.querySelector('form.x1ed109x').click();");
 
                 IWebElement textElement = wait.Until(ExpectedConditions.ElementExists(By.CssSelector("form.x1ed109x p.xdj266r")));
-                textElement.SendKeys("Yörük Travel");
+                textElement.SendKeys("Yörük Travel" + " "+
+                    "https://img.freepik.com/free-photo/full-shot-travel-concept-with-landmarks_23-2149153258.jpg?3&w=1060&t=st=1696840426~exp=1696841026~hmac=f5d7d7a8e3e1b12e0308ab80e0d18a1b1baad92fc1653ec341c53d9ca957068c");
+
+                //"https://www.youtube.com/watch?time_continue=2&v=B2CAprs8Mnw"
 
 
                 js.ExecuteScript("document.querySelector('form.x1ed109x .x9f619.x1n2onr6.x1ja2u2z.x78zum5.xdt5ytf.x2lah0s.x193iq5w.xeuugli.xsyo7zv.x16hj40l.x10b6aqq.x1yrsyyn:nth-child(2) .x1ey2m1c').click()");
